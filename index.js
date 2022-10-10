c52559cd4693aaa68592040251168f915004008a
Shamun <shamunahmed38@gmail.com>
2022-10-10 22:15:11 +0600

    background done. left and right container being worked on

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('<h1>Chat app</h1>');
-});
-
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });
+app.use(express.static('public'));
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
