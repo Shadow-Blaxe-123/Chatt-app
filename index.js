3f5c7c804e069ac2e1ba70f588d2e7d09c88dac1
Shamun <shamunahmed38@gmail.com>
2022-10-12 22:26:41 +0600

    Starting main logic started ont the back-end

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const path = require('path');
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/public/index.html'));
   });
 app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
